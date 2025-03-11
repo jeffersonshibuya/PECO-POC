923a2f2774544c6f1d42cc92419e9804fabab4d1
Jefferson R. Shibuya <jeffersonshibuya@yahoo.com.br>
2025-03-11 15:26:00 -0300

    Nextjs config api route

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: [
-    {
-      source: "/api/:path*",
-      destination: "/app/api/:path*",
-    },
-  ],
+  rewrites: {
+    source: "/api/:path*",
+    destination: "/app/api/:path*",
+  },
 };
 
 export default nextConfig;
